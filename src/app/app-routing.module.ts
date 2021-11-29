256d86c45637d59886d8dad5865ee7f04e560ef2
Timothy Harper <leeharperdev@gmail.com>
2021-11-29 13:40:41 -0600

    Update app-routing.module.ts
    Updated: app-routing.module.ts.
    -> Implemented a route for viewing the transactions of an account.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginPageComponent } from './components/login-page/login-page.component';
+import { AccountTransactionsPageComponent } from "./components/account-transactions-page/account-transactions-page.component";
 
 const routes: Routes = [
   {
     path: "login",
     component: LoginPageComponent,
-
+  },
+  {
+    path: "accounts/:id/transactions",
+    component: AccountTransactionsPageComponent
   }
 ];
 
